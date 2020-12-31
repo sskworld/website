a0d32021b5c2ed4a861f8c6cb5e1000c3f09677c
Suraj Kale <suraj.kale@solacetechnologies.co.in>
2020-12-31 11:31:57 +0530

    data

---
+++ precache-manifest.37281096bebaae096358bd9d2ff477e7.js
@@ -0,0 +1,162 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f33ad85f24826ecc591a8b1891f8483a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f9e9011fd01e88a9be75",
+    "url": "/static/css/2.bae238e4.chunk.css"
+  },
+  {
+    "revision": "30bff696477a77cd59cc",
+    "url": "/static/css/main.5e4f79e5.chunk.css"
+  },
+  {
+    "revision": "f9e9011fd01e88a9be75",
+    "url": "/static/js/2.8dd6e42f.chunk.js"
+  },
+  {
+    "revision": "30bff696477a77cd59cc",
+    "url": "/static/js/main.231cfb2d.chunk.js"
+  },
+  {
+    "revision": "ae48cc4bde4a0d02970f",
+    "url": "/static/js/runtime~main.3e0faf37.js"
+  },
+  {
+    "revision": "9a2a854272e473f4c038494e6c39fbd7",
+    "url": "/static/media/banner1.9a2a8542.jpg"
+  },
+  {
+    "revision": "34dc00a77b7afd5453b3050167a306ce",
+    "url": "/static/media/banner2.34dc00a7.jpg"
+  },
+  {
+    "revision": "093d9e2c2f42701cb6817613bc394a19",
+    "url": "/static/media/banner3.093d9e2c.jpg"
+  },
+  {
+    "revision": "f5fed46bafc1b70404c28b5f63160443",
+    "url": "/static/media/contact.f5fed46b.jpg"
+  },
+  {
+    "revision": "75e4d6d5a0f6b8f4e4030f9b00511ca7",
+    "url": "/static/media/css.75e4d6d5.png"
+  },
+  {
+    "revision": "e397baaf65d5d4c0d5fca7b9b4853df4",
+    "url": "/static/media/customer.e397baaf.png"
+  },
+  {
+    "revision": "5433ade830f23784a38918ef4145cf3e",
+    "url": "/static/media/delivery.5433ade8.png"
+  },
+  {
+    "revision": "892b3f33789a99517849e9373b4761b6",
+    "url": "/static/media/facebook.892b3f33.png"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "0dee1e816e863e77964e5b98141b6088",
+    "url": "/static/media/global.0dee1e81.png"
+  },
+  {
+    "revision": "31039a53ab57f00b4eff33fb7bcfa2e8",
+    "url": "/static/media/html.31039a53.png"
+  },
+  {
+    "revision": "454f1e5f8f0c8bad6b7f7e19976be0bc",
+    "url": "/static/media/idea.454f1e5f.png"
+  },
+  {
+    "revision": "bd0ff8dc625f8558a5a43989b844d6c2",
+    "url": "/static/media/instagram.bd0ff8dc.png"
+  },
+  {
+    "revision": "589922e187ab719d0afa9c4c2993019b",
+    "url": "/static/media/linkedin.589922e1.png"
+  },
+  {
+    "revision": "165c30a123d05247ab80dab7d518a66b",
+    "url": "/static/media/logo.165c30a1.png"
+  },
+  {
+    "revision": "1d9d2c38d1a9e6e6eff27827b86aef50",
+    "url": "/static/media/mail.1d9d2c38.png"
+  },
+  {
+    "revision": "55e83371dbf50a5f027b1fe31a054ad3",
+    "url": "/static/media/node.55e83371.png"
+  },
+  {
+    "revision": "4b6d55ed6055d6852d3acc536e5e2b7f",
+    "url": "/static/media/office1.4b6d55ed.jpg"
+  },
+  {
+    "revision": "504f53ed6d34926e440fb414a428ff7c",
+    "url": "/static/media/other-service.504f53ed.png"
+  },
+  {
+    "revision": "9cdfc090c25790dbc68ce69eed9f4807",
+    "url": "/static/media/page.9cdfc090.jpg"
+  },
+  {
+    "revision": "a59a44e0a7eb8ef26c626c2a5094d38d",
+    "url": "/static/media/passion.a59a44e0.png"
+  },
+  {
+    "revision": "5883f03e29da1b502dd067e9273bbf58",
+    "url": "/static/media/pattern.5883f03e.jpg"
+  },
+  {
+    "revision": "f297dfcd8b3e9215ee862f232d5c9d13",
+    "url": "/static/media/react.f297dfcd.png"
+  },
+  {
+    "revision": "936f7052c38a735c5052258f7a5514b2",
+    "url": "/static/media/services.936f7052.jpg"
+  },
+  {
+    "revision": "afe6f9ee55d340cf55c212b15685eee6",
+    "url": "/static/media/smartphone.afe6f9ee.png"
+  },
+  {
+    "revision": "69041a8b584beb70f137764db4b4b4ab",
+    "url": "/static/media/testing.69041a8b.png"
+  },
+  {
+    "revision": "b9e3c20ea2e2320c9c51488b61e9f38c",
+    "url": "/static/media/translate.b9e3c20e.png"
+  },
+  {
+    "revision": "519e42fac59aa69959a03d3793ac12ac",
+    "url": "/static/media/user.519e42fa.png"
+  },
+  {
+    "revision": "107e6ee967c4c55336e22326dbef0b59",
+    "url": "/static/media/value.107e6ee9.png"
+  },
+  {
+    "revision": "f87c2a95a859e76a88304cf273b56f03",
+    "url": "/static/media/why.f87c2a95.jpg"
+  }
+]);
